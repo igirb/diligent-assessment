2fcedcde4e94742d0500c3548990d7f5e7837738
igirb <139497578+igirb@users.noreply.github.com>
2024-10-21 16:51:20 +0200

    FEATURE: recipe details command implemented and validated

--- src/commands.ts
+++ src/commands.ts
@@ -10,3 +10,26 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
   console.log('Your recipes:');
   console.log(formatted);
 }
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+
+  if (args.length !== 1) {
+    console.error('Error: The details command requires exactly one argument, the recipe ID.');
+    return;
+  }
+
+  const id = Number(args[0]);
+
+  if (!Number(id)) {
+    console.error('Error: Recipe ID must be a numeric value.');
+  } else if(!recipes[id]) {
+    console.error(`Error: Recipe with ID ${id} not found.`);
+  }
+
+  const searchedRecipe = recipes[id];
+
+  console.log(`ID: ${searchedRecipe.id}`);
+  console.log(`Name: ${searchedRecipe.name}`);
+}
