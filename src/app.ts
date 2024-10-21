cc94b192a967a32f2e6f9db84de705fdcdf7ae2f
igirb <139497578+igirb@users.noreply.github.com>
2024-10-21 16:28:17 +0200

    ERRORHANDLING: log error when given command is unknown

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,6 @@ export async function createApp(store: Store<RecipeType[]>, args: string[], ) {
     const commandFunction = commands[command] 
     await commandFunction(store, restArgs);
   } else {
-    throw new AppError(`Unknown command: ${command}`);
+    console.error(`Unknown command: ${command}`);
   }
 }
